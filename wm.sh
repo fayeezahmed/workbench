e3394a4305aa618a5a5e76550393b5022eeb590f
Fayeez Ahmed <fayeez.ahmed@news.co.uk>
2022-06-28 16:37:41 +0100

    added window management script and config files for yabai & skhdrc

---
+++ wm.sh
@@ -0,0 +1,39 @@
+function installPackage(){
+  packageName=$1
+  packagePath=$2
+
+  echo "packageName: $packageName"
+  echo "packagePath: $packagePath"
+  if brew ls --versions $packageName > /dev/null; then
+    # The package is installed
+    brew services start $packagePath
+  else
+    # The package is not installed
+    echo "$packageName is already installed!"
+    if [ ! -z "$packagePath"]; then
+      echo "Starting $packageName"
+      brew install $packagePath
+    fi
+  fi
+}
+
+###########
+## YABAI ##
+###########
+installPackage yabai koekeishiya/formulae/yabai
+
+##########
+## SKHD ##
+##########
+installPackage skhd koekeishiya/formulae/skhd
+
+##############
+## SPACEBAR ##
+##############
+#installPackage spacebar cmacrae/formulae/spacebar
+#brew install homebrew/cask-fonts/font-fontawesome
+
+##########################
+## ALACRITTY - Terminal ##
+##########################
+brew install --cask alacritty
