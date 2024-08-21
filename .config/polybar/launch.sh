4d82769823889e4d835b4b0b94248fbe75cd9d19
Fayeez Ahmed <fayeez.ahmed@globalrelay.net>
2024-08-21 11:51:19 +0100

    Update to polybar minor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,10 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
+if [[$1 == 1]]; then
+    echo ".... FC_DEBUG enabled ...."
+fi
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
@@ -14,9 +18,10 @@ for m in $(polybar --list-monitors | cut -d":" -f1); do
     if [[ "$m" == "eDP-1" ]]; then
         continue;
     fi
-    FC_DEBUG=1 MONITOR=$m polybar --reload external --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+    FC_DEBUG=$1 MONITOR=$m polybar --reload external --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
 done
-FC_DEBUG=1 polybar --reload laptop --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+
+FC_DEBUG=$1 polybar --reload laptop --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
 
 
 echo "Bars launched..."
